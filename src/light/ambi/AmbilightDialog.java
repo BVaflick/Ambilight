f26d7082491548c7a932a175c18ac2f17dc00bf0
BVaflick <benvaflick@gmail.com>
2015-08-24 21:48:17 +0300

    Added rectPanel as a field of Dialog

--- src/light/ambi/AmbilightDialog.java
+++ src/light/ambi/AmbilightDialog.java
@@ -2,6 +2,8 @@ package light.ambi;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class AmbilightDialog extends JDialog {
 
@@ -9,19 +11,52 @@ public class AmbilightDialog extends JDialog {
 
     private int height;
 
+    private int coordinateX;
+
+    private int coordinateY;
+
+    private AmbilightRectanglePanel rectanglePanel;
+
+
     public AmbilightDialog(int width, int height) {
         this.width = width;
         this.height = height;
         setSize(width, height);
+
+        this.rectanglePanel = new AmbilightRectanglePanel(this.getSize(), 6, 4);
+
         setUndecorated(true);
         setLocationRelativeTo(null);
-        //setVisible(true);
         getRootPane().setOpaque(false);
-        setBackground(new Color(28, 38, 46,130));
+        setBackground(new Color(28, 38, 46, 130));
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        add(this.rectanglePanel);
+
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                coordinateX = e.getX();
+                coordinateY = e.getY();
+            }
+        });
+
+        addMouseMotionListener(new MouseAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                setLocation((getX() + (e.getX() - coordinateX)) > ((GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().width)-222) ?
+                                ((GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().width)-202) : (getX() + (e.getX() - coordinateX)),
+                        (getY() + (e.getY() - coordinateY)) > ((GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().height)- 138) ?
+                                ((GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().height)-118) : (getY() + (e.getY() - coordinateY)));
+            }
+        });
     }
-    public void setToLowerRight(){
+
+    public void setToLowerRight() {
         Rectangle screenBounds = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
         setLocation(screenBounds.width - this.width, screenBounds.height - this.height);
+    }
+
+    public AmbilightRectanglePanel getRectanglePanel() {
+        return rectanglePanel;
     }
 }
